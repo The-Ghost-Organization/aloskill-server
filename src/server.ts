d8f3c6b591618e07f4b2b52f335a76c3d686f88c
sumaiya ahmed <sumu0612@gmail.com>
2025-09-13 21:33:45 +0600

    first commit testytuy

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ dotenv.config();
 
 const PORT = process.env.PORT ?? 5000;
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`✅ Server is running at http://localhost:${PORT}`);
 });
